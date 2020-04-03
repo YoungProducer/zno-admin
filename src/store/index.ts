53f627857d278cd9993bbd328de8609116932557
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-03 16:36:34 +0300

    Changed creating of redux-store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,56 +9,90 @@ import {
     getDefaultMiddleware,
 } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
+import { History } from 'history';
 
 // Application's imports
 import rootReducer from './slices';
+import Api from 'api';
+import { IApi } from 'api/types';
+import history from 'routes/history';
 
-const midlleware = getDefaultMiddleware({
-    thunk: true,
-    serializableCheck: ({
-        ignoredActions: [
-            /**
-             * This actions work with File's.
-             * To prevent error about non-serializable values need to ignore its.
-             */
-            'TaskBuffer/setBufferTaskImageAction',
-            'TaskBuffer/setBufferExplanationImageAction',
-            'TasksList/addTaskAction',
-            'TasksList/deleteTaskAction',
-            'TasksList/changeTaskImageAction',
-            'TasksList/changeExplanationImageAction',
-            /**
-             * This actions in some cases can work with react component.
-             * To prevent errors its must be added to ignoredActions.
-             */
-            'Notifier/closeSnackbarAction',
-            'Notifier/enqueueSnackbarAction',
-            'Notifier/removeSnackbarAction',
-            'TasksList/toggleOpenTasksListAction',
-            'SubjectConfigurations/setSubjectNameAction',
-            'SubjectConfigurations/setSubSubjectNameAction',
-            'SubjectConfigurations/setThemeNameAction',
-        ],
-        ignoredPaths: [
-            'createTest.taskBuffer.taskImage',
-            'createTest.taskBuffer.explanationImage',
-            'createTest.tasksList.tasks',
-        ],
-    }),
-    immutableCheck: ({
-        ignore: ['createTest'],
-    }),
-});
+export interface ThunkExtraArgument {
+    history: History<{}>;
+    api: IApi;
+}
 
-const logger = createLogger({
-    collapsed: true,
-    diff: true,
-});
+const createStore = () => {
+    /** Extract env variable */
+    const production = process.env.NODE_ENV === 'production';
+    const development = process.env.NODE_ENV === 'development';
 
-const store = configureStore({
-    reducer: rootReducer,
-    middleware: [...midlleware, logger],
-});
+    /** Create Api instance */
+    const api = new Api();
+
+    /** Define middlewares */
+    const defaultMiddleware = getDefaultMiddleware({
+        thunk: {
+            extraArgument: {
+                history,
+                api,
+            },
+        },
+        serializableCheck: ({
+            ignoredActions: [
+                /**
+                 * This actions work with File's.
+                 * To prevent error about non-serializable values need to ignore its.
+                 */
+                'TaskBuffer/setBufferTaskImageAction',
+                'TaskBuffer/setBufferExplanationImageAction',
+                'TasksList/addTaskAction',
+                'TasksList/deleteTaskAction',
+                'TasksList/changeTaskImageAction',
+                'TasksList/changeExplanationImageAction',
+                /**
+                 * This actions in some cases can work with react component.
+                 * To prevent errors its must be added to ignoredActions.
+                 */
+                'Notifier/closeSnackbarAction',
+                'Notifier/enqueueSnackbarAction',
+                'Notifier/removeSnackbarAction',
+                'TasksList/toggleOpenTasksListAction',
+                'SubjectConfigurations/setSubjectNameAction',
+                'SubjectConfigurations/setSubSubjectNameAction',
+                'SubjectConfigurations/setThemeNameAction',
+            ],
+            ignoredPaths: [
+                'createTest.taskBuffer.taskImage',
+                'createTest.taskBuffer.explanationImage',
+                'createTest.tasksList.tasks',
+            ],
+        }),
+        immutableCheck: ({
+            ignore: ['createTest'],
+        }),
+    });
+
+    const logger = !production
+        ? require('redux-logger').createLogger({
+            collapsed: true,
+            diff: true,
+        })
+        : undefined;
+
+    const middleware = production
+        ? [...defaultMiddleware]
+        : [...defaultMiddleware, logger];
+
+    return configureStore({
+        middleware,
+        reducer: rootReducer,
+    });
+};
+
+const store = createStore();
+
+export type Store = typeof store;
 
 export type AppDispatch = typeof store.dispatch;
 
