5275c4f927974d5f503c51e5a351a537ee8ec0f4
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-04 00:08:32 +0300

    Fixed TaskConfiguration container.

--- src/components/CreateTest/TaskConfigurations/container.ts
+++ src/components/CreateTest/TaskConfigurations/container.ts
@@ -9,31 +9,33 @@ import { connect } from 'react-redux';
 
 // Application's imports
 import {
-    setBufferTaskTypeAction, ETaskType,
-} from 'store/slices/createTest/taskBuffer';
+    TaskSlice,
+    setTaskTypeAction,
+} from 'store/slices/task';
 import {
-    selectTaskBuffer,
-} from 'store/selectors/createTest';
+    selectTaskType,
+} from 'store/selectors/task';
 import { RootState } from 'store/slices';
 
 interface IOwnProps {}
 
 interface IStateProps {
-    taskType: ETaskType;
+    taskType: TaskSlice.TaskType;
 }
 
 interface IDispatchProps {
-    setTaskType?: (type: ETaskType) => void;
+    setTaskType?: (type: TaskSlice.TaskType) => void;
 }
 
 export type TTaskConfigurationProps = IOwnProps & IStateProps & IDispatchProps;
 
 const mapStateToProps = (state: RootState): IStateProps => ({
-    ...selectTaskBuffer(state),
+    taskType: selectTaskType(state),
 });
 
 const mapDispatchToProps = (dispatch: any): IDispatchProps => ({
-    setTaskType: (type: ETaskType) => dispatch(setBufferTaskTypeAction(type)),
+    setTaskType: (type: TaskSlice.TaskType) =>
+        dispatch(setTaskTypeAction(type)),
 });
 
 export default connect<IStateProps, IDispatchProps, IOwnProps>(
