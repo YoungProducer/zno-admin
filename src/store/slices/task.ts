6a669fc621b7fc6895e8695501d233218b96fdaf
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-04 12:59:05 +0300

    Added new actions for task slice which allows to add and delete images.

--- src/store/slices/task.ts
+++ src/store/slices/task.ts
@@ -8,8 +8,24 @@
 /** External imports */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+export interface Image extends File {
+    /** Blob string */
+    preview: string;
+}
+
 export namespace TaskSlice {
     export type TaskType = 'SINGLE' | 'RELATIONS' | 'TEXT';
+
+    export interface Task {
+        answer: string[];
+        type: TaskType;
+        /** Related only for text type of answer */
+        answersAmount: number;
+        /** Task image */
+        image: Image;
+        /** Explanation image */
+        explanationImage: Image;
+    }
 
     export interface SetAnswerPayload {
         /**
@@ -23,18 +39,15 @@ export namespace TaskSlice {
         answer: string;
     }
 
-    export interface State {
-        answer: string[];
-        type: TaskType;
-        /** Related only for text type of answer */
-        answersAmount: number;
-    }
+    export interface State extends Task {}
 }
 
 const initialState: TaskSlice.State = {
     answer: [''],
     type: 'SINGLE',
     answersAmount: 1,
+    image: null,
+    explanationImage: null,
 };
 
 const task = createSlice({
@@ -79,6 +92,55 @@ const task = createSlice({
                 ? state.answer.concat([''])
                 : state.answer.slice(0, payload),
         }),
+        setTaskImageAction: (
+            state: TaskSlice.State,
+            { payload }: PayloadAction<File>,
+        ) => ({
+            ...state,
+            image: Object.assign(payload, {
+                preview: URL.createObjectURL(payload),
+            }),
+        }),
+        deleteTaskImageAction: (
+            state: TaskSlice.State,
+        ) => {
+            // Delete reference to this object in browser to prevent memory leak
+            URL.revokeObjectURL(state.image.preview);
+
+            return {
+                ...state,
+                image: null,
+            };
+        },
+        setExplanationImageAction: (
+            state: TaskSlice.State,
+            { payload }: PayloadAction<File>,
+        ) => ({
+            ...state,
+            explanationImage: Object.assign(payload, {
+                preview: URL.createObjectURL(payload),
+            }),
+        }),
+        deleteExplanationImageAction: (
+            state: TaskSlice.State,
+        ) => {
+            // Delete reference to this object in browser to prevent memory leak
+            URL.revokeObjectURL(state.explanationImage.preview);
+
+            return {
+                ...state,
+                image: null,
+            };
+        },
+        /** Sets all properties to default */
+        clearTaskAction: (state: TaskSlice.State) => {
+            URL.revokeObjectURL(state.image.preview);
+            URL.revokeObjectURL(state.explanationImage.preview);
+
+            return {
+                ...initialState,
+            };
+        },
     },
 });
 
@@ -86,6 +148,11 @@ export const {
     setTaskTypeAction,
     setAnswerAction,
     setAnswersAmountAction,
+    setTaskImageAction,
+    deleteTaskImageAction,
+    setExplanationImageAction,
+    deleteExplanationImageAction,
+    clearTaskAction,
 } = task.actions;
 
 export default task.reducer;
