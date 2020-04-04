4017dd14c52d06a23a3ea7da205663fc16efd573
YoungProducer <sashabezrukovownmail@gmail.com>
2020-04-04 13:05:16 +0300

    Added new selectors for Task slice.

--- src/store/selectors/task.ts
+++ src/store/selectors/task.ts
@@ -5,6 +5,9 @@
  * Selectors for task slice.
  */
 
+/** External imports */
+import { createSelector } from '@reduxjs/toolkit';
+
 /** Application's imports */
 import { RootState } from 'store/slices';
 
@@ -16,3 +19,25 @@ export const selectTaskAnswer = (state: RootState) =>
 
 export const selectTaskAnswersAmount = (state: RootState) =>
     state.task.answersAmount;
+
+export const selectTaskImage = (state: RootState) =>
+    state.task.image;
+
+export const selectExplanationImage = (state: RootState) =>
+    state.task.explanationImage;
+
+export const selectImagesPreviews = createSelector(
+    [selectTaskImage, selectExplanationImage],
+    (taskImage, explanationImage) => ({
+        taskImagePreview: taskImage.preview,
+        explanationImagePreview: explanationImage.preview,
+    }),
+);
+
+export const selectImagesNames = createSelector(
+    [selectTaskImage, selectExplanationImage],
+    (taskImage, explanationImage) => ({
+        taskImageName: taskImage.name,
+        explanationImageName: explanationImage.name,
+    }),
+);
